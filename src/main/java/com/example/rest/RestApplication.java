818514e087cf00ba738e27f2381e36f3455f8329
nadeeshan1 <kavindunadeeshan2002@gmail.com>
2026-03-31 18:56:19 +0530

    git commit -m

--- src/main/java/com/example/rest/RestApplication.java
+++ src/main/java/com/example/rest/RestApplication.java
@@ -2,12 +2,26 @@ package com.example.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestApplication.class, args);
+    }
 
+    @GetMapping("/")
+    public String rootEndpoint() {
+        String message = "Hello, world!";
+        return message;
+    }
+
+    @GetMapping("/hello")
+    public String helloWithName(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return "Hello, " + name + "!";
+    }
 }
